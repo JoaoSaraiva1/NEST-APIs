4a2ea6425dd978296e97d78250ed9b3f21db71c5
João Saraiva <joao.msaraiva@ename.pt>
2023-10-18 18:02:48 +0100

    Ids para items Paris 2023. Script utiliza cada id para obter o valor do item

--- steam/scraper/buff163.py
+++ steam/scraper/buff163.py
@@ -1,5 +1,33 @@
 import requests
-res= requests.get('https://buff.163.com/api/market/goods/sell_order?game=csgo&goods_id=33895').json()
+import json
 
-for price in res['data']['items']:
-    print(price['price'])
+# Open the file and read the goods IDs
+with open('./paris_2023.txt', 'r') as file:
+    lines = file.readlines()
+
+goods_ids = [line.split(';')[0] for line in lines]
+
+# Iterate over each goods ID
+for goods_id in goods_ids:
+    url = f'https://buff.163.com/api/market/goods/sell_order?game=csgo&goods_id={goods_id}'
+    
+    try:
+        res = requests.get(url)
+        res.raise_for_status()  # Raise an exception for HTTP errors
+        
+        try:
+            data = res.json()
+            
+            if 'items' in data['data']:
+                items = data['data']['items']
+                
+                if items:
+                    print(f"Price for goods ID {goods_id}: {items[0]['price']}")
+                else:
+                    print(f"No items found for goods ID {goods_id}.")
+            else:
+                print(f"No 'items' key found in the response for goods ID {goods_id}.")
+        except json.JSONDecodeError:
+            print(f"Invalid JSON response for goods ID {goods_id}.")
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data for goods ID {goods_id}: {e}")
