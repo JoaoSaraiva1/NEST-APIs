19f1b9373df3e14c93f074f4bda82744a032f69c
Joao Saraiva <joaosaraiva.s.js@gmail.com>
2023-10-19 22:02:03 +0100

    Melhora velocidade para obter os precos. Retry pode nao estar a 100%

--- steam/scraper/set_price_history.py
+++ steam/scraper/set_price_history.py
@@ -2,6 +2,8 @@ import psycopg2
 import requests
 import json
 from datetime import date
+from concurrent.futures import ThreadPoolExecutor
+import time
 
 # Replace these with your actual database connection details
 db_params = {
@@ -13,48 +15,72 @@ db_params = {
 }
 
 # Define a function to fetch buff_ids from the database and update price history
-def fetch_and_update_price_history(conn):
+# Adjust the number of threads according to your system's capabilities
+NUM_THREADS = 6
+
+# Define a function to fetch and update price history for a single buff_id
+def fetch_and_update_price(buff_id, conn, max_retries=5):
+    retries = 0
+    while retries < max_retries:
+        try:
+            url = f'https://buff.163.com/api/market/goods/sell_order?game=csgo&goods_id={buff_id}'
+            res = requests.get(url)
+            res.raise_for_status()
+
+            data = res.json()
+            if 'items' in data['data']:
+                items = data['data']['items']
+
+                if items:
+                    price = items[0]['price']
+                    today = date.today()
+
+                    # Collect data for batch insert
+                    batch_data.append((buff_id, price, today))
+                    print(f"Price for goods ID {buff_id}: {price} added to batch.")
+                    return  # Success, exit the function
+                else:
+                    print(f"No items found for goods ID {buff_id}.")
+            else:
+                print(f"No 'items' key found in the response for goods ID {buff_id}.")
+        except (requests.exceptions.RequestException, json.JSONDecodeError) as e:
+            print(f"Error fetching data for goods ID {buff_id}: {e}")
+
+        # Increment retries and apply exponential backoff
+        retries += 1
+        wait_time = 2 ** retries  # Exponential backoff: 2^retries seconds
+        time.sleep(wait_time)
+
+    print(f"Max retries reached for goods ID {buff_id}")
+
+# Define the batch_insert_to_db function
+def batch_insert_to_db(conn, data):
     try:
         cur = conn.cursor()
-        cur.execute("SELECT buff_id FROM items")
-        buff_ids = [row[0] for row in cur.fetchall()]
-        
-        for buff_id in buff_ids:
-            url = f'https://buff.163.com/api/market/goods/sell_order?game=csgo&goods_id={buff_id}'
-            try:
-                res = requests.get(url)
-                res.raise_for_status()  # Raise an exception for HTTP errors
-                
-                try:
-                    data = res.json()
-                    if 'items' in data['data']:
-                        items = data['data']['items']
-                        
-                        if items:
-                            price = items[0]['price']
-                            today = date.today()
-                            
-                            # Insert the data into the "price_history" table
-                            cur.execute("INSERT INTO price_history (buff_id, price, date) VALUES (%s, %s, %s)", (buff_id, price, today))
-                            print(f"Price for goods ID {buff_id}: {price} added to price history.")
-                        else:
-                            print(f"No items found for goods ID {buff_id}.")
-                    else:
-                        print(f"No 'items' key found in the response for goods ID {buff_id}.")
-                except json.JSONDecodeError:
-                    print(f"Invalid JSON response for goods ID {buff_id}.")
-            except requests.exceptions.RequestException as e:
-                print(f"Error fetching data for goods ID {buff_id}: {e}")
-        
+        cur.executemany("INSERT INTO price_history (buff_id, price, date) VALUES (%s, %s, %s)", data)
         conn.commit()
-        cur.close()
+        print(f"Batch of {len(data)} records inserted into the database.")
     except Exception as e:
-        print("Error:", e)
+        print("Error inserting into the database:", e)
         conn.rollback()
 
 if __name__ == "__main__":
     try:
         conn = psycopg2.connect(**db_params)
-        fetch_and_update_price_history(conn)
+        cur = conn.cursor()
+        cur.execute("SELECT buff_id FROM items")
+        buff_ids = [row[0] for row in cur.fetchall()]
+
+        # Initialize data for batch insert
+        batch_data = []
+
+        with ThreadPoolExecutor(max_workers=NUM_THREADS) as executor:
+            for buff_id in buff_ids:
+                executor.submit(fetch_and_update_price, buff_id, conn)
+
+        # Perform batch inserts into the database
+        batch_insert_to_db(conn, batch_data)
+
+        cur.close()
     except psycopg2.Error as e:
         print("Error connecting to the database:", e)
