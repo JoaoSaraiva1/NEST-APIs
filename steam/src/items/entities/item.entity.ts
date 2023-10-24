00eeca005ea311fb1558df91b1a1f6807e517b72
João Saraiva <joao.msaraiva@ename.pt>
2023-10-24 12:26:07 +0100

    Corrige intem entity

--- steam/src/items/entities/item.entity.ts
+++ steam/src/items/entities/item.entity.ts
@@ -1,13 +1,7 @@
 // src/items/entities/item.entity.ts
 import { PriceHistory } from 'src/price_history/entities/price_history.entity';
-import { PurchasedItem } from 'src/purchased_items/entities/purchased_item.entity';
-import {
-  Entity,
-  PrimaryGeneratedColumn,
-  Column,
-  ManyToMany,
-  JoinTable,
-} from 'typeorm';
+import { PurchasedItems } from 'src/purchased_items/entities/purchased_item.entity';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToMany } from 'typeorm';
 
 @Entity({ name: 'items' })
 export class Item {
@@ -15,8 +9,7 @@ export class Item {
   id: number;
 
   @ManyToMany(() => PriceHistory)
-  @ManyToMany(() => PurchasedItem)
-  @JoinTable()
+  @ManyToMany(() => PurchasedItems)
   @Column()
   buff_id: string;
 
